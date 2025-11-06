e63abd5ec82dead6151d0f0b167d2a434a467ec1
Moha <matsailali2425@politecnics.barcelona>
2025-11-06 15:30:06 +0100

    recuperacion datos

---
+++ app/src/main/java/com/example/myapplication/GameActivity.kt
@@ -0,0 +1,18 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class GameActivity {
+    class GameActivity : AppCompatActivity() {
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            enableEdgeToEdge()
+            setContentView(R.layout.game_activity)
+
+        }
+    }
+}
